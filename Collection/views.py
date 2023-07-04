a152fb4545da350f7420274ca4fa4d106322c351
orcan_og <tricyxyt@gmail.com>
2023-07-04 15:42:09 +0200

    Modification de views.py

--- Collection/views.py
+++ Collection/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests
 
 # Create your views here.
-
-def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
 
 def accueil(request):
     return render(request, 'Collection/templates/accueil.html')
@@ -17,5 +13,4 @@ def favori(request):
     return render(request, 'Collection/templates/favori.html')
 
 def recherche(request):
-    return HttpResponse('<h1>PAGE RECHERCHE</h1>')
-    #return render(request, 'Collection/templates/recherche.html')
+    return render(request, 'Collection/templates/recherche.html')
