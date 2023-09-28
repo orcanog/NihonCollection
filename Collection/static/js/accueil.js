319d7e6c994880e469fdf2e4ae04df6fb4e23af4
Atlas21278 <imzilenabdelali2004@gmail.com>
2023-09-29 01:35:42 +0200

    Mise à jour de la page de connexion et d'enregistrement

--- Collection/static/js/accueil.js
+++ Collection/static/js/accueil.js
@@ -9,3 +9,13 @@ profile.addEventListener('click', function(event) {
 document.addEventListener('click', function() {
     profileMenu.classList.remove('show');
 });
+
+
+const searchContainer = document.querySelector('.search-container');
+const searchIcon = document.getElementById('search-git icon');
+const searchInput = document.getElementById('search-input');
+
+searchIcon.addEventListener('click', () => {
+    searchContainer.classList.toggle('active');
+    searchInput.focus();
+});
