5fa0ff757836beb546d7a118b1c230c4d40a29f4
orcan_og <tricyxyt@gmail.com>
2023-10-13 10:56:56 +0200

    Correction des erreurs majeures

--- Collection/static/js/accueil.js
+++ Collection/static/js/accueil.js
@@ -1,5 +1,3 @@
-const profileMenu = document.getElementById('profileMenu');
-const profile = document.getElementById('profile');
 const searchContainer = document.querySelector('.search-container');
 const searchIcon = document.getElementById('search-git icon');
 const searchInput = document.getElementById('search-input');
@@ -7,15 +5,6 @@ const searchBar = document.querySelector('#search-form');
 const animeAPI = document.querySelector(".image-container");
 const animeResult = document.querySelector(".result-container");
 let timerId;
-
-profile.addEventListener('click', function(event) {
-    event.stopPropagation();
-    profileMenu.classList.toggle('show');
-});
-
-document.addEventListener('click', function() {
-    profileMenu.classList.remove('show');
-});
 
 /**searchIcon.addEventListener('click', () => {
     searchContainer.classList.toggle('active');
